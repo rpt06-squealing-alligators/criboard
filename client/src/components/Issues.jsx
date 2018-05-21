b401836a3d74be7d36d8cb081a7e0ef4c94a247f
Adelle Housker <housker@gmail.com>
2018-05-21 17:48:37 -0500

    added Newissue component

--- client/src/components/Issues.jsx
+++ client/src/components/Issues.jsx
@@ -4,14 +4,28 @@ import ReactDOM from 'react-dom';
 class Issues extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      showReportButton: false
+    }
+    this.displayReportButton = this.displayReportButton.bind(this);
   }
+
+  displayReportButton() {
+    this.setState({showReportButton: !this.state.showReportButton})
+    console.log('this.state.showReportButton: ', this.state.showReportButton)
+  }
+
   render() {
     return(
       <div className="grid">
-        <h3>Reports</h3>
-        <h3>Scheduled</h3>
-        <h3>In Progress</h3>
-        <h3>Fixed</h3>
+        <h3 className="reports" onMouseEnter={this.displayReportButton}>Reports</h3>
+          {
+            this.state.showReportButton &&
+            <a href="/newissue" className="reports-body">Report New Issue</a>
+          }
+        <h3 className="scheduled">Scheduled</h3>
+        <h3 className="ip">In Progress</h3>
+        <h3 className="fixed">Fixed</h3>
       </div>
     )
   }
