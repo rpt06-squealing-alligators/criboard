09382701d5ab58c5a3892f9a90e3240bca7cb814
Adelle Housker <housker@gmail.com>
2018-05-21 12:13:01 -0500

    components folder and Issues.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Issues from './components/Issues.jsx'
 require('./assets/styles/index.css')
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        Hello World
+        <Issues />
       </div>
     )
 
