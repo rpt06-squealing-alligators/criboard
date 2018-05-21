0acf404fffe90c161e7457db7d4857f9cb1b2aa2
Adelle Housker <housker@gmail.com>
2018-05-21 13:28:50 -0500

    changed nodemon to node in test

--- server/spec/server-spec.js
+++ server/spec/server-spec.js
@@ -3,7 +3,7 @@ var expect = require('chai').expect;
 
 describe('server', function() {
   it('should respond to GET requests for / with a 200', function(done) {
-    request('http://127.0.0.1:3000/', function(error, response, body) {
+    request('http://127.0.0.1:3000', function(error, response, body) {
       expect(response.statusCode).to.equal(200);
       done();
     });
