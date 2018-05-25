5586f1bf9234566dfa3ed5cb53144a48b0d21603
Jared Horne <thomhorne@gmail.com>
2018-05-24 20:42:49 -0500

    added functionality to general info page

--- client/src/components/GeneralInfo.jsx
+++ client/src/components/GeneralInfo.jsx
@@ -3,7 +3,23 @@ import ReactDOM from 'react-dom';
 
 
 class GeneralInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
 
+    }
+  }
+  render () {
+    <div>
+      <h3>General Info</h3>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+    </div>
+  }
 }
 
 export default GeneralInfo;
