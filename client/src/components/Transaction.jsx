dc7bdcb2c5e7a090f4e9c4529eb86f5f6c3fbde6
Amena <syedaamenakauser@gmail.com>
2018-05-27 12:28:03 -0400

    Add a dropdown for users while entering a new transaction

--- client/src/components/Transaction.jsx
+++ client/src/components/Transaction.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Home from '../components/Home.jsx';
 
 import { Button } from 'react-bootstrap';
 
@@ -9,7 +10,8 @@ class Transaction extends React.Component {
     this.state = {
       bill: '',
       amount: '',
-      person: ''
+      person: '',
+      people: []
     }
   }
 
@@ -19,30 +21,57 @@ class Transaction extends React.Component {
     });
   }
 
+  componentDidMount() {
+    axios.get('/fetchusers')
+      .then((result) => {
+        // console.log(result)
+        this.setState({
+          people: result.data,
+          person: result.data[0]
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
   onSubmit() {
     axios.post('/addtransaction', this.state)
-    .then(res => console.log(res))
-    // console.log(this.state)
-    // TODO - send info to server
-
+    .then(res => {
+      // console.log(res)
+      alert('Transaction has been posted');
+    });
   }
 
   render() {
+    let optionItems = this.state.people.map(person => {
+      return (
+        <option key={person}>
+          {person}
+        </option>
+      );
+    });
     return (
       <div>
-        <div className="form-group">
-          <label>Bill</label>&nbsp;&nbsp;
-          <input type="text" className="form-control" placeholder="Enter bill you are paying" name="bill" value={this.state.bill} onChange={this.onChange.bind(this)} />
+        <Home />
+        <div className="jumbotron">
+          <h1 className="display-4">Enter a transaction</h1>
+          <div className="form-group">
+            <label>Bill</label>&nbsp;&nbsp;
+            <input type="text" className="form-control" placeholder="Enter bill you are paying" name="bill" value={this.state.bill} onChange={this.onChange.bind(this)} />
+          </div>
+          <div className="form-group">
+            <label>Amount</label>&nbsp;&nbsp;
+            <input type="text" className="form-control" placeholder="Enter amount" name="amount" value={this.state.amount} onChange={this.onChange.bind(this)} />
+          </div>
+          <div className="form-group">
+            <label>Paid by</label>&nbsp;&nbsp;
+            <select className="form-control" name="person" value={this.state.person} onChange={this.onChange.bind(this)} >
+              {optionItems}
+            </select>
+          </div>
+          <Button className="btn btn-primary" onClick={this.onSubmit.bind(this)}>Submit</Button>
         </div>
-        <div className="form-group">
-          <label>Amount</label>&nbsp;&nbsp;
-          <input type="text" className="form-control" placeholder="Enter amount" name="amount" value={this.state.amount} onChange={this.onChange.bind(this)} />
-        </div>
-        <div className="form-group">
-          <label>Paid by</label>&nbsp;&nbsp;
-          <input type="text" className="form-control" placeholder="Paid by" name="person" value={this.state.person} onChange={this.onChange.bind(this)} />
-        </div>
-        <Button className="btn btn-primary" onClick={this.onSubmit.bind(this)}>Submit</Button>
       </div>
     );
   }
