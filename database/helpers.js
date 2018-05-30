8f779ad2637db99be7ed00c899db6f24642f6e0c
Amena <syedaamenakauser@gmail.com>
2018-05-29 22:53:35 -0400

    Add a function in database to settle up the given 2 users

--- database/helpers.js
+++ database/helpers.js
@@ -22,105 +22,104 @@ var createUser = (username, email, password, callback) => {
   User.findOne({
     where: {username: username}
   })
-    .then((result) => {
-      // console.log(result)
-      // if user doesn't exist
-      if (result === null) {
-        User.create({
-          username: username, email: email, password: password
+  .then((result) => {
+    // console.log(result)
+    // if user doesn't exist
+    if (result === null) {
+      User.create({
+        username: username, email: email, password: password
+      })
+      .then((result) => {
+        // console.log('new user created', result)
+        // grab the last inserted id in database
+        User.findOne({
+          where: {username: username}
         })
-          .then((result) => {
-            // console.log('new user created', result)
-            // grab the last inserted id in database
-            User.findOne({
-              where: {username: username}
-            })
-              .then((result) => {
-                //login comes from passport and creates a session and a cookie for the user
-                var user_id = result.dataValues.id;
-                callback(true, user_id);
-              })
-          })
-      } else {
-        // username already exists in database
-        // console.log('username already exists');
-        callback(false);
-      }
-    })
+        .then((result) => {
+          //login comes from passport and creates a session and a cookie for the user
+          var user_id = result.dataValues.id;
+          callback(true, user_id);
+        })
+      })
+    } else {
+      // username already exists in database
+      // console.log('username already exists');
+      callback(false);
+    }
+  })
 };
 
 var authenticateUser = function(username, password, isMatch) {
   User.findOne({
     where: {username: username}
   })
-    .then((result) => {
-      if (result === null) {
-        // console.log('user does not exist');
-        isMatch(false);
-      } else {
-        var hash = result.dataValues.password;
-        // console.log('hash for user', hash);
-        var userId = result.dataValues.id;
-        bcrypt.compare(password, hash, function(err, response) {
-          if (response === true) {
-            isMatch(true, userId);
-          } else {
-            isMatch(false);
-          }
-        });
-      }
-    })
-    .catch(err => {
-      // console.log('error reading from database');
-      isMatch(false)
-    })
+  .then((result) => {
+    if (result === null) {
+      // console.log('user does not exist');
+      isMatch(false);
+    } else {
+      var hash = result.dataValues.password;
+      // console.log('hash for user', hash);
+      var userId = result.dataValues.id;
+      bcrypt.compare(password, hash, function(err, response) {
+        if (response === true) {
+          isMatch(true, userId);
+        } else {
+          isMatch(false);
+        }
+      });
+    }
+  })
+  .catch(err => {
+    // console.log('error reading from database');
+    isMatch(false)
+  })
 };
 
 var fetchUsers = function(callback) {
   User.findAll({})
-    .then(result => {
-      var people = result.map(person => {
-        return person.dataValues.username;
-      })
-      // console.log(people)
-      callback(people);
+  .then(result => {
+    var people = result.map(person => {
+      return person.dataValues.username;
     })
-    .catch(err => {
-      // console.log('error fetching from database');
-      callback(null);
-    })
+    // console.log(people)
+    callback(people);
+  })
+  .catch(err => {
+    // console.log('error fetching from database');
+    callback(null);
+  })
 };
 
 // function to get all data from transactions table
 var fetchActivity = function(callback) {
   var transactions = [];
   Transaction.findAll({})
-    .then(results => {
-      results.forEach(result => {
-        User.findOne({
-          where: {id: result.UserId}
-        })
-          .then(user => {
-            username = user.dataValues.username;
-            var transaction = {
-              paidBy: username,
-              amount: result.amount,
-              bill: result.bill
-            };
-            transactions.push(transaction);
-            if (transactions.length === results.length) {
-              // console.log(transactions)
-              callback(transactions);
-            }
-          })
+  .then(results => {
+    results.forEach(result => {
+      User.findOne({
+        where: {id: result.UserId}
+      })
+      .then(user => {
+        username = user.dataValues.username;
+        var transaction = {
+          paidBy: username,
+          amount: result.amount,
+          bill: result.bill
+        };
+        transactions.push(transaction);
+        if (transactions.length === results.length) {
+          // console.log(transactions)
+          callback(transactions);
+        }
       })
     })
-    .catch(err => {
-      // console.log(err);
-      callback(null)
-    });
+  })
+  .catch(err => {
+    // console.log(err);
+    callback(null)
+  });
 };
-
 
 var reportIssue = (title, description, image) => {
   // console.log('title: ', title)
@@ -147,19 +146,20 @@ var initGroup = () => {
   fetchUsers(users => {
     var n = users.length;
     var groupTable = [];
-  for (var i = 0; i < n; i++) {
-    groupTable[i] = []
-    for (var j = 0; j < n; j++) {
-      groupTable[i][j] = 0;
+    for (var i = 0; i < n; i++) {
+      groupTable[i] = []
+      for (var j = 0; j < n; j++) {
+        groupTable[i][j] = 0;
+      }
     }
-  }
-  var matrix = JSON.stringify(groupTable)
-  Group.create({
-    groupname: 'Super Mario World',
-    matrix: matrix
-  })
+    var matrix = JSON.stringify(groupTable);
+    Group.create({
+      groupname: 'Super Mario World',
+      matrix: matrix
+    })
     .then(result => {
       // console.log('result of initializing group', result)
+      console.log('group initialized');
     })
   })
 };
@@ -261,6 +261,43 @@ var findUserInfo = function(username, callback) {
   })
 };
 
+// given a username, find user id in table
+var findUserId = function(username, callback) {
+  User.findOne({
+    where: {username: username}
+  })
+  .then(result => {
+    // console.log(result.dataValues.id)
+    var userId = result.dataValues.id
+    callback(userId)
+  })
+}
+
+// make values of 2 matrix elements 0 - these elements are determined from the given user ids
+var settleUsers = function(id1, id2, callback) {
+  Group.findOne({
+    where: {groupname: 'Super Mario'}
+  })
+  .then(result => {
+    var groupMatrix = result.dataValues.matrix;
+    var groupTable = JSON.parse(groupMatrix);
+    // console.log('USERS IN DATABASE', id1, id2)
+    groupTable[id1][id2] = 0;
+    groupTable[id2][id1] = 0;
+    groupMatrix = JSON.stringify(groupTable);
+    Group.update({
+      matrix: groupMatrix
+    }, {
+      where: {
+        groupname: 'Super Mario World'
+      }
+    })
+    .then(result => {
+      callback(true);
+    })
+  })
+};
+
 
 module.exports = {
   createUser: createUser,
@@ -273,5 +310,7 @@ module.exports = {
   updateLedger: updateLedger
   fetchUsers: fetchUsers,
   fetchActivity: fetchActivity,
-  findUserInfo: findUserInfo
+  findUserInfo: findUserInfo,
+  findUserId: findUserId,
+  settleUsers: settleUsers
 };
