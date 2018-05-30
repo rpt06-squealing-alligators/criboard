812f6f1a6d31207c04d6803247c03edd003db81a
Amena <syedaamenakauser@gmail.com>
2018-05-29 23:08:15 -0400

    Modify Dashboard component to display info specific to logged in user and allow users to settle up

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -7,8 +7,11 @@ class Dashboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: ''
-    }
+      user: '',
+      finances: [],
+      groupUsers: []
+    };
+    this.onClick = this.onClick.bind(this);
   }
 
   componentDidMount() {
@@ -16,12 +19,53 @@ class Dashboard extends React.Component {
       .then(result => {
         console.log('logged in user', result.data);
         this.setState({
-          user: result.data
+          user: result.data.username,
+          finances: result.data.row,
+          groupUsers: result.data.users
         });
       })
   }
 
+  onClick(i) {
+    // console.log('button clicked', this.state.user, i)
+    var params = {
+      user1: this.state.user,
+      user2: i
+    };
+    axios.post('/settleup', params)
+      .then(result => {
+        console.log(result);
+        axios.get('/getuserinfo')
+          .then(result => {
+            console.log('logged in user', result.data);
+            this.setState({
+              user: result.data.username,
+              finances: result.data.row,
+              groupUsers: result.data.users
+            });
+          })
+        })
+  }
+
   render() {
+    var itemsOwedByUser = this.state.groupUsers.map((item, i) => {
+      if (this.state.finances[i] > 0) {
+        return (
+          <tr key={i}>
+            <td>{this.state.user} owes ${this.state.finances[i]} to {item}<button className="settle" onClick={() => this.onClick(i)}>Settle Up</button></td>
+          </tr>
+        );
+      }
+    });
+    var itemsOwedToUser = this.state.groupUsers.map((item, i) => {
+      if (this.state.finances[i] < 0) {
+        return (
+          <tr key={i}>
+            <td> {this.state.user} is owed ${Math.abs(this.state.finances[i])} by {item}<button className="settle" onClick={() => this.onClick(i)}>Settle Up</button></td>
+          </tr>
+        );
+      }
+    });
     return(
       <div>
       <Home />
@@ -32,6 +76,18 @@ class Dashboard extends React.Component {
           <li>User specific info</li>
           <li>Whiteboard</li>
         </ul>
+        <table className="table table-hover">
+          <thead><tr><th>{this.state.user} Owes</th></tr></thead>
+            <tbody>
+            {itemsOwedByUser}
+          </tbody>
+        </table>
+        <table className="table table-hover">
+          <thead><tr><th>{this.state.user} is Owed</th></tr></thead>
+            <tbody>
+            {itemsOwedToUser}
+          </tbody>
+        </table>
       </div>
     )
   }
