952e1bada22b6fe214b28249e8776505d4e8d3d5
Amena <syedaamenakauser@gmail.com>
2018-06-02 17:16:29 -0400

    Fix merge conflicts

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -19,36 +19,21 @@ class Dashboard extends React.Component {
   componentDidMount() {
     axios.get('/getaddress')
     .then(result => {
+      console.log('result.data', result.data)
       this.setState({
       user: result.data.username,
       address: result.data.address,
       latitude: result.data.latitude,
       longitude: result.data.longitude
-      }, () => {
-        var mymap = L.map('mapid').setView([this.state.latitude, this.state.longitude], 15);
-        var marker = L.marker([this.state.latitude, this.state.longitude]).addTo(mymap);
-        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      });
+      var mymap = L.map('mapid').setView([this.state.latitude, this.state.longitude], 15);
+      var marker = L.marker([this.state.latitude, this.state.longitude]).addTo(mymap);
+      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             maxZoom: 18,
             id: 'mapbox.streets',
             accessToken: 'pk.eyJ1IjoiaG91c2tlciIsImEiOiJjamh2aXMwODcwem5uM2twMzA3cmZsbnBvIn0.rz3s-qyoAcFzzrOd91YdYg'
         }).addTo(mymap);
-
-      });
-
-      // axios.get('/getaddress')
-      // .then(result => {
-      //   // result.data should have latitude, longitude, address
-          // use this to setState
-
-
-
-      // });
-      // axios.get('/getaddress')
-      // .then(result => {
-      //   // result.data should have latitude, longitude, address
-          // use this to setState
-      // })
     });
 
   }
@@ -65,7 +50,7 @@ class Dashboard extends React.Component {
           <li>Notifications</li>
           <li>User specific info</li>
         </ul>
-        <textarea className="whiteboard" name="message" rows="10" cols="30" defaultValue={this.state.whiteboard}></textarea>
+       {/*} <textarea className="whiteboard" name="message" rows="10" cols="30" defaultValue={this.state.whiteboard}></textarea> */}
       </div>
       </div>
     )
