a58e74fb59d3f9ec6f7791d1d4b8714858f5d2e1
Amena <syedaamenakauser@gmail.com>
2018-06-02 13:51:30 -0400

    Include address fields in users table, add an Address component

--- database/models/user.js
+++ database/models/user.js
@@ -12,6 +12,18 @@ var User = db.define('User', {
   },
   password: {
     type: Sequelize.STRING
+  },
+ latitude: {
+    type: Sequelize.DECIMAL,
+    default: 42.09
+  },
+  longitude: {
+    type: Sequelize.DECIMAL,
+    default: -72.58
+  },
+  address: {
+    type: Sequelize.STRING,
+    default: "123 Main St; Springfield, MA 01105"
   }
 });
 
